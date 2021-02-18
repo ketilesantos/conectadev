d5ccda77bf89e2e9d12492fa4231341b07273f1c
Ketile <ketile.pimenta@gmail.com>
2021-02-18 08:11:38 -0300

    add new commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import { makeStyles } from '@material-ui/core/styles';
+
 import Header from './components/Header'
+import Feed from './components/Feed'
+import NavBar from './components/NavBar'
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    main: {
+        height: '100vh',
+        display: 'flex',
+        width:  '1200px',
+        margin: '0 auto'
+    }
+})
 
 function Home (){
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.root}>
             <Header />
-            <main className="main">
-               <div className="navbar">
-               </div>
-               <div className="feed"></div>
+            <main className={classes.main}>
+               <NavBar />
+              <Feed />
             </main>
         </div>
     )
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,20 +1,61 @@
 import React from 'react'
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { SvgIcon } from '@material-ui/core';
+import {Bell} from 'react-feather'
+import { Avatar } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    appBar: {
+        boxShadow: 'none',
+    },
+    img: {
+        maxHeight:55
+    },
+    grow: {
+        flexGrow: 1
+    },
+    userSection: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    button: {
+        marginRight: 10
+    },
+    bell:{
+        marginRight: 10
+    }
+})
 
 function Header () {
+    const classes = useStyles()
     return(
-        <header className="header">
-            <div  className="toolbar">
-                <div>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+            <Toolbar >
+
+                <img src="/images/logo.png" alt="Logo" className={classes.img}/>
+                <div className={classes.grow}></div>
+                <div className={classes.userSection}>
+                     <Button  variant="contained" color="primary" className={classes.button} >Novo Post</Button>
+                     <SvgIcon className={classes.bell}>
+                         <Bell></Bell>
+                     </SvgIcon>
+                     <Avatar alt="Remy Sharp" src="/" />
+                </div>
+
+                {/* <div>
                     <h1>Conecta dev</h1>
                 </div>
             
                 <div>
-                    <button>Novo Post</button>
+                    <Button  variant="contained" color="primary" >Novo Post</Button>
                     <span>im1 </span>
                     <span>im2 </span>
-                </div>
-            </div>
-        </header>
+                </div> */}
+            </Toolbar>
+        </AppBar>
     )
 }
 
